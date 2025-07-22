b6cbf859e6067e20c5135ed3d6699a528ceb5801
Vũ Hoàng <153354779+hoangvu201@users.noreply.github.com>
2025-07-22 22:36:52 +0700

    Update StudentServiceImpl.java

--- Project_Java/src/ra/managementSoftware/business/impl/StudentServiceImpl.java
+++ Project_Java/src/ra/managementSoftware/business/impl/StudentServiceImpl.java
@@ -1,8 +1,18 @@
 package ra.managementSoftware.business.impl;
 
 import ra.managementSoftware.business.IStudentService;
+import ra.managementSoftware.dao.IStudentDAO;
+import ra.managementSoftware.dao.impl.StudentDAOImpl;
 
 public class StudentServiceImpl implements IStudentService {
+    private final IStudentDAO studentDAO;
 
+    public StudentServiceImpl() {
+        studentDAO = new StudentDAOImpl();
+    }
 
+    @Override
+    public boolean checkLoginStudent(String email, String password) {
+        return studentDAO.loginStudent(email, password);
+    }
 }
